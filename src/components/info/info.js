319990ef3cf48bb7b7257e1825ec03a21328bf80
Vadim Manushin <vadimmanushin@yandex.ru>
2023-09-03 14:54:03 +0300

    add handling of close info block when click outside

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -1,25 +1,35 @@
-const openInfo = (event) => {
-  let description;
-  let plus;
-  let minus;
-  let root;
-  if (event.target.classList.contains('info') || event.target.parentNode.classList.contains('info')) {
-    if (event.target.classList.contains('info')) {
-      root = event.target;
-    } else {
-      root = event.target.parentNode;
-    }
-    [, plus, minus, description] = root.childNodes;
-    if (description.classList.contains('info_invisible')) {
-      description.classList.remove('info_invisible');
-      minus.classList.remove('info_invisible');
-      plus.classList.add('info_invisible');
-    } else {
-      description.classList.add('info_invisible');
-      plus.classList.remove('info_invisible');
-      minus.classList.add('info_invisible');
-    }
+const handleInfoClick = (root) => {
+  const [, plus, minus, description] = root.childNodes;
+  if (description.classList.contains('info_invisible')) {
+    description.classList.remove('info_invisible');
+    minus.classList.remove('info_invisible');
+    plus.classList.add('info_invisible');
+  } else {
+    description.classList.add('info_invisible');
+    plus.classList.remove('info_invisible');
+    minus.classList.add('info_invisible');
   }
 };
 
-document.addEventListener('pointerdown', (event) => openInfo(event));
+const closeInfo = (root) => {
+  const [, plus, minus, description] = root.childNodes;
+  description.classList.add('info_invisible');
+  plus.classList.remove('info_invisible');
+  minus.classList.add('info_invisible');
+};
+
+const handleInfo = (event) => {
+  let infoRootElem;
+  if (event.target.classList.contains('info') || event.target.parentNode.classList.contains('info')) {
+    if (event.target.classList.contains('info')) {
+      infoRootElem = event.target;
+    } else {
+      infoRootElem = event.target.parentNode;
+    }
+    handleInfoClick(infoRootElem);
+  } else {
+    document.querySelectorAll('.info').forEach((infoRoot) => closeInfo(infoRoot));
+  }
+};
+
+document.addEventListener('pointerdown', (event) => handleInfo(event));
